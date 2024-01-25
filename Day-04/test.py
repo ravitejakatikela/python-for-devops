63c28a8c26e501b14c246cebf16eddaf9b659681
ravitejakatikela <150242614+ravitejakatikela@users.noreply.github.com>
2024-01-25 13:34:21 +0100

    first commit

---
+++ Day-04/test.py
@@ -0,0 +1,11 @@
+def addition(num1,num2):
+    add = num1 + num2
+    return add
+
+def subtraction(num1,num2)
+    sub = num1 - num2
+    return sub
+
+print("The output of addition is",str(addition(10,5)))
+print("The output of subtraction is",str(subtraction(10,5)))
+      
